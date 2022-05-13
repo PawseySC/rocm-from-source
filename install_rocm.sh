0ce14cf211b9aca4fcbe17b9332ff0ca9df7413f
Cristian Di Pietrantonio <cristiandipietrantonio@gmail.com>
2022-05-13 23:54:13 +0000

    rocBLAS installation successful.

--- install_rocm.sh
+++ install_rocm.sh
@@ -19,61 +19,61 @@ export CPATH=$ROCM_INSTALL_DIR/rocclr/include/elf:$ROCM_INSTALL_DIR/include/hsa:
 
 
 function export_vars {
-	export LD_LIBRARY_PATH=$1/lib:$LD_LIBRARY_PATH
-	export LIBRARY_PATH=$1/lib:$LIBRARY_PATH
-	export LD_LIBRARY_PATH=$1/lib64:$LD_LIBRARY_PATH
-	export LIBRARY_PATH=$1/lib64:$LIBRARY_PATH
-	export PATH=$1/bin:$PATH
-	export CPATH=$1/include:$CPATH
-	export CPATH=$1/inc:$CPATH
-	export CMAKE_PREFIX_PATH=$1:$CMAKE_PREFIX_PATH
+    export LD_LIBRARY_PATH=$1/lib:$LD_LIBRARY_PATH
+    export LIBRARY_PATH=$1/lib:$LIBRARY_PATH
+    export LD_LIBRARY_PATH=$1/lib64:$LD_LIBRARY_PATH
+    export LIBRARY_PATH=$1/lib64:$LIBRARY_PATH
+    export PATH=$1/bin:$PATH
+    export CPATH=$1/include:$CPATH
+    export CPATH=$1/inc:$CPATH
+    export CMAKE_PREFIX_PATH=$1:$CMAKE_PREFIX_PATH
 }
 
 
 
 function run_command {
-	echo "Running command $@"
-	# All the following mess is due to the ';' present in some parameters. We need to put arguments to cmake into single quotes.
-	declare -i count
-	count=0
-	string_to_eval=""
-	for arg in $@;
-	do
-	if [ $count -eq 0 ]; then
-			string_to_eval="$arg"
-	else
-			string_to_eval="$string_to_eval '$arg'"
-	fi
-	(( count=count+1 ))
-	done
-	eval "$string_to_eval"
-	if [ $? -ne 0 ]; then
-	    echo "Error running a command: $@"
-       	exit 1
-	fi	       
+    echo "Running command $@"
+    # All the following mess is due to the ';' present in some parameters. We need to put arguments to cmake into single quotes.
+    declare -i count
+    count=0
+    string_to_eval=""
+    for arg in $@;
+    do
+    if [ $count -eq 0 ]; then
+            string_to_eval="$arg"
+    else
+            string_to_eval="$string_to_eval '$arg'"
+    fi
+    (( count=count+1 ))
+    done
+    eval "$string_to_eval"
+    if [ $? -ne 0 ]; then
+        echo "Error running a command: $@"
+           exit 1
+    fi           
 }
 
 
 
 function cmake_install {
-	cd $BUILD_FOLDER
-	PACKAGE_NAME="$1"
-	CMAKE_FLAGS="-DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_INSTALL_PREFIX=$ROCM_INSTALL_DIR"
-	SOURCE_DIR=".."
+    cd $BUILD_FOLDER
+    PACKAGE_NAME="$1"
+    CMAKE_FLAGS="-DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_INSTALL_PREFIX=$ROCM_INSTALL_DIR"
+    SOURCE_DIR=".."
     if [ $# -eq 2 ]; then
-		CMAKE_FLAGS="$2"
-	fi
-	echo "Installing $PACKAGE_NAME .."
-	cd $PACKAGE_NAME
-	if [ "$PACKAGE_NAME" = "ROCR-Runtime" ]; then
-		cd src
-	elif [ "$PACKAGE_NAME" = "llvm-project" ]; then
+        CMAKE_FLAGS="$2"
+    fi
+    echo "Installing $PACKAGE_NAME .."
+    cd $PACKAGE_NAME
+    if [ "$PACKAGE_NAME" = "ROCR-Runtime" ]; then
+        cd src
+    elif [ "$PACKAGE_NAME" = "llvm-project" ]; then
         SOURCE_DIR="../llvm"
     fi
-	mkdir build 
-	cd build
-	run_command cmake "$CMAKE_FLAGS" "$SOURCE_DIR"
-	run_command make -j $NCORES install
+    mkdir build 
+    cd build
+    run_command cmake "$CMAKE_FLAGS" "$SOURCE_DIR"
+    run_command make -j $NCORES install
     cd $BUILD_FOLDER
 }
 
@@ -107,14 +107,14 @@ if [[ `which pip` == "" ]]; then
 fi
 cd $BUILD_FOLDER
 if ! [ -d hipamd ]; then 
-	# Dowload all the ROCM repositories with the repo tool. First, we need the tool
+    # Dowload all the ROCM repositories with the repo tool. First, we need the tool
     run_command curl https://storage.googleapis.com/git-repo-downloads/repo -o "$BUILD_FOLDER/bin/repo"
     run_command chmod a+x "$BUILD_FOLDER/bin/repo"
     echo "Downloading ROCM repositories"
     run_command repo init -u https://github.com/RadeonOpenCompute/ROCm.git -b roc-${ROCM_VERSION_BRANCH}
     run_command repo sync
-	# Needed for rocBLAS
-	run_command git clone -b cpp-3.0.1 https://github.com/msgpack/msgpack-c.git
+    # Needed for rocBLAS
+    run_command git clone -b cpp-3.0.1 https://github.com/msgpack/msgpack-c.git
     
 fi
 
@@ -183,7 +183,7 @@ BITCODE_DIR=$ROCM_INSTALL_DIR/llvm/amdgcn/bitcode
 #     echo gfx908
 
 #     """ > rocm_agent_enumerator
-#     chmod 0777 rocm_agent_enumerator
+#     chmod 0755 rocm_agent_enumerator
 # fi
 
 export HIP_CLANG_PATH=$ROCM_INSTALL_DIR/llvm/bin
@@ -195,8 +195,8 @@ export HIP_ROCCLR_HOME=$ROCM_INSTALL_DIR/hip/rocclr
 export HIP_RUNTIME=rocclr
 
 # cmake_install hipamd "-DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DHIP_COMMON_DIR=$COMMON_HIP -DCMAKE_PREFIX_PATH=$BUILD_FOLDER/rocclr;$ROCM_INSTALL_DIR\
-# 	-DROCM_PATH=$ROCM_INSTALL_DIR -DCMAKE_INSTALL_PREFIX=$ROCM_INSTALL_DIR/hip -DHSA_PATH=$ROCM_INSTALL_DIR/hsa -DROCCLR_PATH=$ROCCLR_DIR \
-# 	-DAMD_OPENCL_PATH=$OPENCL_DIR  -DCMAKE_HIP_ARCHITECTURES=$GFX_ARCHS"
+#     -DROCM_PATH=$ROCM_INSTALL_DIR -DCMAKE_INSTALL_PREFIX=$ROCM_INSTALL_DIR/hip -DHSA_PATH=$ROCM_INSTALL_DIR/hsa -DROCCLR_PATH=$ROCCLR_DIR \
+#     -DAMD_OPENCL_PATH=$OPENCL_DIR  -DCMAKE_HIP_ARCHITECTURES=$GFX_ARCHS"
 
 # # revert back previous hack
 # if [ ${SYSTEM_HAS_GPU} -eq 0 ]; then
@@ -211,16 +211,16 @@ export HIP_RUNTIME=rocclr
 # cmake_install HIPIFY "-DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=$ROCM_INSTALL_DIR/hipify"
 # cmake_install ROCdbgapi
 # cmake_install rocr_debug_agent "-DCMAKE_MODULE_PATH=$ROCM_INSTALL_DIR/hip/cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
-# 	-DCMAKE_INSTALL_PREFIX=$ROCM_INSTALL_DIR -DCMAKE_HIP_ARCHITECTURES=$GFX_ARCHS"
+#     -DCMAKE_INSTALL_PREFIX=$ROCM_INSTALL_DIR -DCMAKE_HIP_ARCHITECTURES=$GFX_ARCHS"
 # cmake_install rocm_bandwidth_test
 # cmake_install half
 
 # build rocblas (CURRENTLY DOES NOT WORK https://github.com/ROCmSoftwarePlatform/rocBLAS/issues/1250)
 # cmake_install msgpack-c "-DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=$ROCM_INSTALL_DIR -DMSGPACK_BUILD_TESTS=OFF -DMSGPACK_BUILD_EXAMPLES=OFF"
 
-cmake_install rocBLAS "-DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_PREFIX_PATH=$ROCM_INSTALL_DIR/llvm;$ROCM_INSTALL_DIR;$ROCM_INSTALL_DIR/hip\
-  	-DCMAKE_TOOLCHAIN_FILE=../toolchain-linux.cmake -DTensile_CODE_OBJECT_VERSION=V3 \
- 	-DCMAKE_INSTALL_PREFIX=$ROCM_INSTALL_DIR  -DAMDGPU_TARGETS=$GFX_ARCHS"
+# cmake_install rocBLAS "-DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_PREFIX_PATH=$ROCM_INSTALL_DIR/llvm;$ROCM_INSTALL_DIR;$ROCM_INSTALL_DIR/hip\
+#      -DCMAKE_TOOLCHAIN_FILE=../toolchain-linux.cmake -DRUN_HEADER_TESTING=OFF -DBUILD_TESTING=OFF -DTensile_CODE_OBJECT_VERSION=V3 \
+#     -DCMAKE_INSTALL_PREFIX=$ROCM_INSTALL_DIR  -DAMDGPU_TARGETS=$GFX_ARCHS"
 # rocRAND
 cmake_install rocRAND "-DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=$ROCM_INSTALL_DIR -DAMDGPU_TARGETS=$GFX_ARCHS"
 
